6074480598d1003193693842da6018713558f9a4
Crowdbotics <team@crowdbotics.com>
2024-09-28 05:00:22 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "automatic-fundme-49527",
+  url: "https://automatic-fundme-49527.botics.co",
+  api: "https://automatic-fundme-49527.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
